9c68e77109d2f282a9801fea93b304e448d2a371
Aaron Stubblefield <ags2189@columbia.edu>
2021-06-18 16:14:22 -0400

    updated notes

--- notes/algebra.py
+++ notes/algebra.py
@@ -3,8 +3,8 @@
 import sympy as sp
 nu = sp.Symbol('nu')
 mu = sp.exp(nu)
-gamma = sp.Symbol('gamma')
-#gamma = 0 # set friction to zero for ice shelf problem
+#gamma = sp.Symbol('gamma')
+gamma = 0 # set background friction to zero for ice shelf or ice stream problems
 
 # matrix for grounded ice
 M = sp.Matrix(( [mu, -1/mu, nu*mu,-nu/mu], [mu, 1/mu, mu*(nu+1),(nu-1)/mu], [1-gamma, 1+gamma, 1-gamma,-1-gamma],[1,1,0,0] ))
@@ -28,11 +28,11 @@ b = sp.Matrix(4,1,[b1,0,b2,b3])
 
 sol, = sp.linsolve((M,b),[A,B,C,D])
 
-# velocity at upper surface of ice sheet
+# vertical velocity at upper surface of ice sheet
 w_h = mu*sol[0] + (1/mu)*sol[1] + nu*mu*sol[2] + (nu/mu)*sol[3]
 
 # print the result (modulo a 1/k factor) for grounded ice:
-sp.pprint(sp.collect( sp.collect(sp.collect(sp.collect(sp.simplify(w_h),b1),b2),b3),mu) )
+#sp.pprint(sp.collect( sp.collect(sp.collect(sp.collect(sp.simplify(w_h),b1),b2),b3),mu) )
 
 # print the result (modulo a 1/k factor) for floating ice:
 #sp.pprint(sp.collect(sp.collect(sp.collect(sp.simplify(w_h),b1),b3),mu) )
@@ -41,3 +41,33 @@ sp.pprint(sp.collect( sp.collect(sp.collect(sp.collect(sp.simplify(w_h),b1),b2),
 # (modulo a 1/k factor)
 #w_b = sol[0]+sol[1]
 #sp.pprint(sp.collect(sp.collect(sp.collect(sp.simplify(w_b),b1),b3),mu) )
+
+
+# For horizontal surface velocity solutions
+# matrix for grounded ice
+M2 = sp.Matrix(( [mu, -1/mu],[1-2*gamma, -(1+2*gamma)]))
+
+theta = sp.Symbol('theta')
+
+
+b4 = -2*(b3 + sol[2]*mu - sol[3]/mu)                   # 1st rhs vector entry
+b5 = -2*(-theta*b2 + b3 + sol[2] - sol[3])                              # 2nd rhs vector entry
+
+# solution vector
+E,F = sp.symbols('E,F')
+
+
+# rhs vector for grounded ice:
+d = sp.Matrix(2,1,[b4,b5])
+
+sol2, = sp.linsolve((M2,d),[E,F])
+
+uh0 = sol2[0]*mu + sol2[1]/mu
+uh1 = K*(sol[0]*(mu-1) + sol[1]*(1-1/mu) + sol[2]*(mu*(1+nu)-1) + sol[3]*((1-nu)/mu - 1))
+
+uh = uh0 + uh1
+
+## print velocity response functions
+#sp.pprint( sp.simplify(sp.collect( sp.collect(sp.collect(sp.collect(sp.simplify(uh),b1),b2),b3),mu) ))
+
+#sp.pprint(sp.simplify(sp.collect(sp.collect(sp.simplify(uh),b1),mu)) )
