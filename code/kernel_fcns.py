2ea8806da85a4b32714521ddcde4b6596f58f240
Aaron Stubblefield <ags2189@columbia.edu>
2021-06-19 16:41:14 -0400

    added surface velocity response functions

--- code/kernel_fcns.py
+++ code/kernel_fcns.py
@@ -1,7 +1,7 @@
 # this file contains the integral kernel functions that are used for applying the
 # forward and adjoint operators
 import numpy as np
-from params import k,kx,beta0,xi,delta
+from params import beta0,xi,delta
 
 def R(k):
     # Ice surface relaxation function for grounded ice
@@ -47,3 +47,48 @@ def B(k):
     D = np.exp(4*n) -2*(1+2*n**2)*np.exp(2*n) + 1
 
     return B1/D
+
+def Uw(k):
+    # Horizontal velocity w-response function
+    n = 2*np.pi*k           # used to convert to SciPy's Fourier Transform definition
+
+    N = np.exp(6*n) - (n+2)*np.exp(5*n) + (2*n**2+4*n+1)*np.exp(4*n)-6*n*np.exp(3*n)\
+        -(2*n**2-4*n+1)*np.exp(2*n)- (n-2)*np.exp(n)-1
+
+    D = np.exp(6*n) + (4*n-1)*np.exp(4*n) - (4*n+1)*np.exp(2*n)+1
+
+    return (2/n)*N/D
+
+def Uh(k):
+    # Horizontal velocity h-response function
+    n = 2*np.pi*k           # used to convert to SciPy's Fourier Transform definition
+
+    N = np.exp(6*n) - n*np.exp(5*n)+(2*n-1)*np.exp(4*n)-(2*n+1)*np.exp(2*n) + n*np.exp(n)+1
+
+    D = np.exp(6*n) + (4*n-1)*np.exp(4*n) - (4*n+1)*np.exp(2*n)+1
+
+    return (2/n**2)*N/D
+
+def Ub(k,kx):
+    # Horizontal velocity beta-response function
+    n = 2*np.pi*k           # used to convert to SciPy's Fourier Transform definition
+    nx = 2*np.pi*kx
+    kap = n**2 / nx**2
+
+    N = np.exp(6*n) - 2*(n+2*kap-1)*np.exp(5*n) + (4*n**2 +1)*np.exp(4*n) \
+        -4*n*(4*kap -1)*np.exp(3*n)-(4*n**2+1)*np.exp(2*n) - 2*(n-2*kap+1)*np.exp(n)-1
+
+    D = np.exp(6*n) + (4*n-1)*np.exp(4*n) - (4*n+1)*np.exp(2*n)+1
+
+    return (kx**2/k**3)*N/D
+
+def Vw(k):
+    return Uw(k)
+
+def Vh(k):
+    return Uh(k)
+
+def Vb(k,kx,ky):
+    nx = 2*np.pi*kx
+    ny = 2*np.pi*ky
+    return (ny/nx)*Ub(k,kx)
