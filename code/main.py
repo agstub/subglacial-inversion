fixed bug in scaling of convolutions, added new example with data from nonlinear model (1 spatial dimension)

code/main.py
@@ -18,15 +18,16 @@
 #   o params.py: set the inversion options and physical/numerical parameters
 #   o synthetic_data.py: create different synthetic data
 #-------------------------------------------------------------------------------
-from params import dim,make_movie,vel_data,save_sol,load_sol
+from params import dim,make_movie,vel_data,save_sol,load_sol,noise_level,nonlin_ex,Nt
 from synthetic_data import h_obs_synth,w_true,beta_true,h_true,u_obs_synth,v_obs_synth
 from inversion import invert
-from plotting import plot_movie,snapshots
+from plotting import plot_movie,snapshots,snapshots_1D
 import numpy as np
 from aux import interp
 from conj_grad import norm
 from operators import sg_fwd
 from scipy.fft import fft2,ifft2
+import os
 
 def main():
     # the default examples use synthetic data (see synthetic_data.py)
@@ -61,7 +62,12 @@ def main():
 
     if dim == 1:
         sol_true = w_true+beta_true
-        snapshots(data,sol,sol_true)
+        if nonlin_ex != 1:
+            snapshots(data,sol,sol_true)
+        else:
+            finn = 'dog'
+            #snapshots_1D(data,fwd,sol,sol_true)
+
 
     elif dim == 2:
         snapshots(data,sol[0],sol[1])
@@ -69,7 +75,21 @@ def main():
     if make_movie == 1:
         plot_movie(sol,fwd,data)
 
-    # save solution as .npy file    
-    np.save('sol.npy',sol)
 
-    return sol
+    dsc = norm(fwd-h_obs)/norm(h_obs)
+
+    print('|h-h_obs|/|h_obs| = '+str(dsc)+' (target = '+str(noise_level)+')')
+
+    # save solution as .npy file
+    if save_sol == 1:
+        np.save('sol.npy',sol)
+
+    return sol,fwd
+
+sol,fwd = main()
+
+if os.path.isdir('pngs_nl')==False:
+    os.mkdir('pngs_nl')    # make a directory for the results.
+
+for i in range(Nt):
+    snapshots_1D(h_obs_synth,fwd,sol,w_true,i)

code/operators.py
@@ -7,7 +7,7 @@
 import numpy as np
 from kernel_fcns import Rg,Tw,Tb,Uw,Uh,Ub,Vw,Vh,Vb
 from params import (uh0,ub0,lamda,eps_beta,eps_w,w_reg,beta_reg,t,k,kx,ky,dx,
-                    inv_w,inv_beta,Nt,nu,dim,u_wt,h_wt,tau,vel_data,Nt)
+                    inv_w,inv_beta,Nt,nu,dim,u_wt,h_wt,tau,vel_data,Nt,t_final)
 from gps_stats import gps_locs
 from scipy.signal import fftconvolve
 from scipy.fft import ifft2,fft2
@@ -15,10 +15,10 @@ from regularizations import reg
 
 #---------------------convolution and cross-correlation operators---------------
 def conv(a,b):
-    return (1/Nt)*fftconvolve(a,b,mode='full',axes=0)[0:Nt,:,:]
+    return (t_final/Nt)*fftconvolve(a,b,mode='full',axes=0)[0:Nt,:,:]
 
 def xcor(a,b):
-    return (1/Nt)*fftconvolve(np.conjugate(np.flipud(a)),b,mode='full',axes=0)[(Nt-1):2*Nt,:,:]
+    return (t_final/Nt)*fftconvolve(np.conjugate(np.flipud(a)),b,mode='full',axes=0)[(Nt-1):2*Nt,:,:]
 
 #-------------------------------------------------------------------------------
 def adj_fwd(X):

code/params.py
@@ -7,6 +7,8 @@ save_sol = int(0)
 
 load_sol = int(0)
 
+nonlin_ex = int(1)
+
 # --------------------inversion options ---------------------------------------
 # set either = 1 (on) or = 0 (off)
 inv_w = int(1)                      # invert for w     (basal vertical velocity)
@@ -30,11 +32,11 @@ h_wt =  1                   # weight on elevation misfit for joint inversion
 #----------------------------regularization-------------------------------------
 # reguarization parameters for each inversion type
 # (default values are optimal according to the discrepancy principle)
-eps_w =  4.27e-5            # w
-eps_beta =  3.319e1         # beta
+eps_w =  1e-1#4.27e-5            # w
+eps_beta =  3.319e1              # beta
 
 # Regularization options: L2 and H1 (see regularizations.py)
-w_reg = 'L2'            # regularization type for w
+w_reg = 'H1'            # regularization type for w
 beta_reg = 'H1'         # regularization type for beta
 
 #---------------------- physical parameters ------------------------------------
@@ -54,19 +56,19 @@ g = 9.81                    # gravitational acceleration
 #-----------------"background flow" (default examples)--------------------------
 
 # slope of basal surface (radians): default 0.2 deg
-slope =  0.2*(np.pi/180.0)
+slope =  0.0*(np.pi/180.0)
 
 # intrinsic surface velocity for inclined slope problem
 uh_slope = (rho_i*g*np.sin(slope)*(H**2)/(2*eta))*np.abs(np.sign(slope))
 
 # surface velocity for simple shear problem
-uh_sshear = 300/3.154e7*(1-np.abs(np.sign(slope)))
+uh_sshear = 0*300/3.154e7*(1-np.abs(np.sign(slope)))
 
 # background sliding velocity (m/s)
-ub = 200/3.154e7
+ub = 0*200/3.154e7
 
 # background horizontal surface velocity (m/s)
-uh = ub*np.abs(np.sign(slope)) + uh_sshear + uh_slope
+uh = 0#ub*np.abs(np.sign(slope)) + uh_sshear + uh_slope
 
 
 # Set background drag coefficient and related parameters, depending on the bed slope
@@ -77,7 +79,7 @@ if slope > 1e-7:
     uz = rho_i*g*np.sin(slope)*H/eta
 else:
     # simple shear problem
-    beta_e = (eta/H)*(uh/ub-1)
+    beta_e = 5.0e9 #(eta/H)*(uh/ub-1)
     uz = (uh - ub)/H
     uzz = 0
 
@@ -113,12 +115,12 @@ noise_level = 0.01         # noise level (scaled relative to elevation anomaly n
 #---------------------- numerical parameters------------------------------------
 cg_tol = 1e-4               # stopping tolerance for conjugate gradient solver
 
-max_cg_iter =  2000         # maximum conjugate gradient iterations
+max_cg_iter =  200         # maximum conjugate gradient iterations
 
 # discretization parameters
-Nx = 101                    # number of grid points in x-direction
-Ny = 101                    # number of grid points in y-direction
-Nt = 100                    # number of time steps
+Nx = 100                    # number of grid points in x-direction
+Ny = 100                    # number of grid points in y-direction
+Nt = 200                    # number of time steps
 
 t0 = np.linspace(0,t_final,num=Nt) # time array
 

code/plotting.py
@@ -3,7 +3,7 @@
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
-from params import x,y,x0,y0,t0,L,inv_w,inv_beta,H,Nt,dim,Nx,vel_data
+from params import x,y,x0,y0,t0,L,inv_w,inv_beta,H,Nt,dim,Nx,vel_data,t_final
 from gps_stats import gps_locs
 from synthetic_data import beta_true,w_true,u_true,v_true,sigma,s_true
 import os
@@ -34,7 +34,7 @@ def plot_movie(sol,fwd,data):
 def plot(sol,h,h_obs,i):
 
     # normalizations for plotting (h,w,beta)
-    sol_true = beta_true+w_true+m_true
+    sol_true = beta_true+w_true
     h_max = np.max([np.max(np.abs(h_obs)),1e-10])
     sol_max = np.max([np.max(np.abs(sol_true)),1e-10])
 
@@ -572,3 +572,56 @@ def gps_plot(sol1,sol2,sol_true):
     plt.show()
     plt.close()
 #-------------------------------------------------------------------------------
+
+
+def snapshots_1D(data,fwd,sol1,sol2,i):
+
+    plt.figure(figsize=(8,10))
+    plt.suptitle(r'$t\, / \, T =$'+"{:.2f}".format(t0[i]/t0[-1]),fontsize=22 )
+    plt.subplot(211)
+    plt.plot(x0,data[i,:,50],color='royalblue',linewidth=3,label=r'data')
+    plt.plot(x0,fwd[i,:,50],color='k',linestyle='--',linewidth=3,label=r'model')
+
+    # plt.plot(x0,data[100,:,50],color='royalblue',linewidth=3)
+    # plt.plot(x0,fwd[100,:,50],color='k',linestyle='--',linewidth=3)
+    #
+    # plt.plot(x0,data[111,:,50],color='royalblue',linewidth=3)
+    # plt.plot(x0,fwd[111,:,50],color='k',linestyle='--',linewidth=3)
+    #
+    # plt.plot(x0,data[122,:,50],color='royalblue',linewidth=3)
+    # plt.plot(x0,fwd[122,:,50],color='k',linestyle='--',linewidth=3)
+
+    plt.ylabel(r'elevation anomaly (m)',fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.ylim(-2,2)
+    plt.gca().xaxis.set_ticklabels([])
+    plt.gca().yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.2f'))
+    plt.legend(fontsize=16,loc='lower right')
+
+    plt.subplot(212)
+
+    plt.plot(x0,sol2[i,:,50],color='royalblue',linewidth=3,label=r'true sol.')
+    plt.plot(x0,sol1[i,:,50],color='k',linestyle='--',linewidth=3,label=r'inversion')
+
+    # plt.plot(x0,sol2[100,:,50],color='royalblue',linewidth=3)
+    # plt.plot(x0,sol1[100,:,50],color='k',linestyle='--',linewidth=3)
+    #
+    # plt.plot(x0,sol2[111,:,50],color='royalblue',linewidth=3)
+    # plt.plot(x0,sol1[111,:,50],color='k',linestyle='--',linewidth=3)
+    #
+    # plt.plot(x0,sol2[122,:,50],color='royalblue',linewidth=3)
+    # plt.plot(x0,sol1[122,:,50],color='k',linestyle='--',linewidth=3)
+
+    plt.ylabel(r'basal vertical vel. (m/yr)',fontsize=16)
+    plt.ylim(-7,7)
+    plt.gca().yaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.2f'))
+    plt.legend(fontsize=16,loc='lower right')
+
+
+    # Label axes and save png:
+    plt.xlabel(r'$x$ (km)',fontsize=20)
+    plt.xticks(fontsize=16)
+    plt.yticks(fontsize=16)
+    plt.tight_layout()
+    plt.savefig('pngs_nl/'+str(i))
+    plt.close()

code/synthetic_data.py
@@ -1,6 +1,6 @@
 # this file creates synthetic data for inversion examples
 
-from params import L,noise_level,x,y,t,Nt,Nx,Ny,inv_w,inv_beta,vel_data,dim,ub0,t_final,dt
+from params import L,noise_level,x,y,t,Nt,Nx,Ny,inv_w,inv_beta,vel_data,dim,ub0,t_final,dt,nonlin_ex
 from operators import forward_w,forward_beta,forward_U,forward_V,sg_fwd
 import numpy as np
 from conj_grad import norm
@@ -19,7 +19,10 @@ sigma = 2*L/3        # standard deviation for Gaussians used in default examples
 # (1) VERTICAL VELOCITY ANOMALY
 # *EXAMPLE 1
 # Subglacial lake : Stationary Gaussian with oscillating amplitude
-w_true = 50*np.exp(-0.5*(sigma**(-2))*(x**2+y**2 ))*np.sin(2*np.pi*t/t_final)*inv_w
+if nonlin_ex != 1:
+    w_true = 5*np.exp(-0.5*(sigma**(-2))*(x**2+0*y**2 ))*np.sin(2*np.pi*t/t_final)*inv_w
+else:
+    w_true = np.load('./input/wb_true.npy')
 
 # *EXAMPLE 2
 # Bed bump: w_b = u_b*ds/dx
@@ -30,7 +33,7 @@ w_true = 50*np.exp(-0.5*(sigma**(-2))*(x**2+y**2 ))*np.sin(2*np.pi*t/t_final)*in
 # (2) SLIPPERINESS ANOMALY
 # Gaussian friction perturbation (constant in time)
 # default is slippery spot, switch sign for sticky spot
-beta_true = -8e-2*np.exp(-0.5*((sigma)**(-2))*(x**2+y**2 ))*np.sin(2*np.pi*box(t)/t_final)*inv_beta
+beta_true = -8e-3*np.exp(-0.5*((sigma)**(-2))*(x**2+y**2 ))*np.sin(2*np.pi*box(t)/t_final)*inv_beta
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -38,12 +41,18 @@ beta_true = -8e-2*np.exp(-0.5*((sigma)**(-2))*(x**2+y**2 ))*np.sin(2*np.pi*box(t
 
 if dim==1 and inv_w == 1:
     #elevation solution
-    h_true = forward_w(w_true)
-    s_true = ifft2(sg_fwd(fft2(w_true))).real
+    if nonlin_ex != 1:
+        h_true = forward_w(w_true)
+        s_true = ifft2(sg_fwd(fft2(w_true))).real
 
-    # velocity solutions
-    u_true = forward_U(w_true,0*beta_true)
-    v_true = forward_V(w_true,0*beta_true)
+        # velocity solutions
+        u_true = forward_U(w_true,0*beta_true)
+        v_true = forward_V(w_true,0*beta_true)
+    else:
+        h_true = np.load('./input/h_true.npy')
+        s_true = 0*h_true
+        u_true = 0*x
+        v_true = 0*x
 
 elif dim==1 and inv_beta == 1:
     #elevation solution
@@ -83,3 +92,11 @@ v_obs_synth = v_true  + 0.1*noise_level*norm(v_true)*noise_v/norm(noise_v)
 # print('max speed = '+str(np.max(np.sqrt(u_true**2 + v_true**2))))
 # print('-----------------------------------------------------------')
 # print('\n')
+
+# # sanity check plotting
+# import matplotlib.pyplot as plt
+# levels = np.linspace(-0.5,0.5,9)
+# plt.contourf(h_obs_synth[100,:,:].T,cmap='coolwarm',vmin=levels[0],vmax=levels[-1],extend='both',levels=levels)
+# plt.colorbar()
+# plt.show()
+# plt.close()